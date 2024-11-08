c084cffb85bf14bf7c39f2455e4a6f775777ed5a
Willian <williansilvaoliveira1234@example.com>
2024-11-08 10:27:21 -0300

    mudando site

--- jsa2.js
+++ jsa2.js
@@ -1,61 +1,9 @@
-		
-		class Carro {
-			
-			constructor(tipo,estagio){
-				
-				this.turbo = new Turbo(estagio)
-				
-				if(tipo==1){
-					
-					this.velmax = 120
-					this.nome = "Esport"
-				}
-				else if (tipo==2){
-					
-					this.velmax = 200
-					this.nome  = "fusca tunado"
-				}
-				else if (tipo==3){
-					
-					this.velmax = 500
-					this.nome ="ferrari"
-				}
-				
-				this.velmax +=this.turbo
-			}
-			
-				info(){
-			console.log(`Velocidade ${this.velmax}`)
-			console.log(`Nome ${this.nome}`)
-			console.log(`Turbo ${this.turbo}`)
-			}
-		}
+	
+	
+		const arr = [1,2,3,4,5]
 		
 		
-		class Turbo{
-			
-			constructor(e){
-				
-				if(e==1){
-					
-					this.pot =100
-				}else if(e==2){
-					
-					this.pot=300
-				}
-				else if(e==3){
-					
-					this.pot = 500
-				}
-				
-				
-				
-			}
-			
+		const value2 = arr.filter(el => el>2)? "trur" : "False";
 		
-			
-		}
+				   console.log(value2.el)
 		
-		const c1 = new Carro(1,1)
-		
-		c1.info();
